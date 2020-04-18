2f280752a6b14e94eec49e061c6dcedaada32503
Hongwei Bai <bhw8412@hotmail.com>
2020-04-18 21:38:14 +1000

    init repo with debug signature

---
+++ app/src/main/java/com/hongwei/bai_fit/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.hongwei.bai_fit.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
---
+++ app/src/main/java/com/hongwei/bai_fit/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.hongwei.bai_fit.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
